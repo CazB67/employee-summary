2742ff0016008f487d39b8e830cb4d7716543f35
Caroline <caroline.portilla@gmail.com>
2020-04-16 14:56:41 +0800

    initial

---
+++ Develop/lib/Manager.js
@@ -0,0 +1,20 @@
+// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+import Employee from "Employee.js";
+/*
+In addition to Employee's properties and methods, Manager will also have:
+officeNumber
+getRole() // Overridden to return 'Manager'
+*/
+class Manager extends Employee
+{
+    constructor(name, id, email, officeNumber)
+    {
+        super(name, id, email);
+        this.officeNumber = officeNumber;
+    }
+    getRole() 
+    {
+        return "Manager";
+    }
+}
+module.exports = Manager;
